5cb74754663480ebad439fbbfdd215dc884a25e5
Ilya Semiankovich <ilya_semiankovich@epam.com>
2018-05-07 14:27:27 +0300

    test hook

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -27,9 +27,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class PetClinicApplication {
-
+    
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(PetClinicApplication.class, args);
+        SpringApplication.run(PetClinicApplication.class, args); //
     }
 
 }
